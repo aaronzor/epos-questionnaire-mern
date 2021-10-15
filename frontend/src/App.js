bf7c4a7c01639921c16c56d90241a5487768cda6
aaron <mcmaaron@gmail.com>
2021-10-15 23:59:05 +0100

    Initialise Project

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from '@mui/material';
+
+import QuestionOne from './Pages/QuestionOne';
+
+const App = () => {
+    return (
+        <Container maxWidth='md'>
+            <QuestionOne />
+        </Container>
+    );
+};
+
+export default App;
