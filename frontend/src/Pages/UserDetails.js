d247389d0a24b565d08aa894417888ca0fb5979c
aaron <mcmaaron@gmail.com>
2021-11-29 21:43:41 +0000

    Validation and routing fixed on userDetails

--- frontend/src/Pages/UserDetails.js
+++ frontend/src/Pages/UserDetails.js
@@ -1,4 +1,4 @@
-import React, { useContext, useCallback } from 'react';
+import React, { useContext, useState } from 'react';
 import {
     Checkbox,
     Container,
@@ -11,7 +11,7 @@ import {
     useMediaQuery,
     TextField
 } from '@mui/material';
-import { Link, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 import NavButton from '../components/NavButton';
 import { ResultContext } from '../contexts/ResultContext';
@@ -24,6 +24,8 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import { motion } from 'framer-motion';
 
 const UserDetails = () => {
+    const [success, setSuccess] = useState(false);
+    let submitSuccess = false;
     const history = useHistory();
     const breakPoint = useMediaQuery((theme) => theme.breakpoints.up('sm'));
     const { object, setObject } = useContext(ResultContext);
@@ -33,149 +35,183 @@ const UserDetails = () => {
         Email: null,
         ContactNumber: null
     };
-
-    const LinkBehavior = React.forwardRef((props, ref) => (
-        <Link ref={ref} to='/endpage' {...props} />
-    ));
-
     const {
         register,
         handleSubmit,
         formState: { errors }
     } = useForm({
+        reValidateMode: 'onBlur',
         resolver: yupResolver(contactInfoSchema)
     });
-
-    const handleOnClick = useCallback(
-        () => history.push('/endpage'),
-        [history]
-    );
 
     const handleOnChange = (event) => {
         const { name, value } = event.target;
         setObject({ ...object, [name]: value });
     };
 
-    const onSubmit = async (object, data) => {
-        await axios.post(`${process.env.REACT_APP_URL}/api/v1/results`, {
-            ContactName: object.ContactName
-        });
+    const onSubmit = async (data) => {
+        // if (!errors.ContactName && !errors.Email && !errors.ContactNumber) {
+        //     history.push('/endpage');
+        // }
+        const send = object;
+        console.log(send);
+        const res = await axios.post(
+            `${process.env.REACT_APP_URL}/api/v1/results`,
+            {
+                send
+            }
+        );
+        console.log(res);
+        // if (res.statusText === 'Created') {
+        //     setSuccess(true);
+        // }
     };
-
+    console.log(success);
     return (
-        <motion.div
-            variants={pageVariants}
-            initial='hidden'
-            animate='visible'
-            exit='exit'
-        >
-            <Container disableGutters maxWidth='xs'>
-                <Box
-                    component='form'
-                    onSubmit={handleSubmit(onSubmit)}
-                    noValidate
+        <div>
+            {!success ? (
+                <motion.div
+                    variants={pageVariants}
+                    initial='hidden'
+                    animate='visible'
+                    exit='exit'
                 >
-                    <Grid
-                        spacing={2}
-                        align='center'
-                        justifyContent='center'
-                        container
-                        marginTop='15%'
-                    >
-                        <Grid item xs={12}>
-                            <Typography>
-                                Please enter your details below
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                error={errors.ContactName}
-                                helperText={
-                                    errors.ContactName && 'Please enter a name'
-                                }
-                                autoFocus
-                                name='ContactName'
-                                id='userDetailsName'
-                                label='Contact Name'
-                                // onChange MUST be below the register when using react-hook-form v7+
-                                {...register('ContactName')}
-                                onChange={handleOnChange}
-                                className={classes.questionInput}
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                error={errors.Email}
-                                helperText={
-                                    errors.Email && 'Please enter a valid email'
-                                }
-                                name='Email'
-                                id='userDetailsEmail'
-                                label='Email'
-                                {...register('Email')}
-                                onChange={handleOnChange}
-                                className={classes.questionInput}
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                error={errors.ContactNumber}
-                                helperText={
-                                    errors.ContactNumber &&
-                                    'Please enter a contact number'
-                                }
-                                name='ContactNumber'
-                                id='userDetailsNumber'
-                                label='Contact Number'
-                                {...register('ContactNumber')}
-                                onChange={handleOnChange}
-                                className={classes.questionInput}
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Typography marginTop='5%'>
-                                Please select your preferred contact method(s)
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <FormGroup
-                                sx={{ paddingLeft: '5%', marginBottom: '3%' }}
+                    <Container disableGutters maxWidth='xs'>
+                        <Box
+                            component='form'
+                            onSubmit={handleSubmit(onSubmit)}
+                            noValidate
+                        >
+                            <Grid
+                                spacing={2}
+                                align='center'
+                                justifyContent='center'
+                                container
+                                marginTop='15%'
                             >
-                                <FormControlLabel
-                                    control={
-                                        <Checkbox onChange={handleOnChange} />
-                                    }
-                                    label='Phone'
-                                    name='Phone Contact'
+                                <Grid item xs={12}>
+                                    <Typography>
+                                        Please enter your details below
+                                    </Typography>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        error={errors.ContactName}
+                                        helperText={
+                                            errors.ContactName &&
+                                            'Please enter a name'
+                                        }
+                                        autoFocus
+                                        name='ContactName'
+                                        id='userDetailsName'
+                                        label='Contact Name'
+                                        // onChange MUST be below the register when using react-hook-form v7+
+                                        {...register('ContactName')}
+                                        onChange={handleOnChange}
+                                        className={classes.questionInput}
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        error={errors.Email}
+                                        helperText={
+                                            errors.Email &&
+                                            'Please enter a valid email'
+                                        }
+                                        name='Email'
+                                        id='userDetailsEmail'
+                                        label='Email'
+                                        {...register('Email')}
+                                        onChange={handleOnChange}
+                                        className={classes.questionInput}
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        error={errors.ContactNumber}
+                                        helperText={
+                                            errors.ContactNumber &&
+                                            'Please enter a contact number'
+                                        }
+                                        name='ContactNumber'
+                                        id='userDetailsNumber'
+                                        label='Contact Number'
+                                        {...register('ContactNumber')}
+                                        onChange={handleOnChange}
+                                        className={classes.questionInput}
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Typography marginTop='5%'>
+                                        Please select your preferred contact
+                                        method(s)
+                                    </Typography>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <FormGroup
+                                        sx={{
+                                            paddingLeft: '5%',
+                                            marginBottom: '3%'
+                                        }}
+                                    >
+                                        <FormControlLabel
+                                            control={
+                                                <Checkbox
+                                                    onChange={handleOnChange}
+                                                />
+                                            }
+                                            label='Phone'
+                                            name='Phone Contact'
+                                        />
+                                        <FormControlLabel
+                                            control={
+                                                <Checkbox
+                                                    onChange={handleOnChange}
+                                                />
+                                            }
+                                            label='Email'
+                                            name='Email Contact'
+                                        />
+                                    </FormGroup>
+                                </Grid>
+                                <NavButton
+                                    variant='back'
+                                    link='/q5'
+                                    clear={clear}
                                 />
-                                <FormControlLabel
-                                    control={
-                                        <Checkbox onChange={handleOnChange} />
-                                    }
-                                    label='Email'
-                                    name='Email Contact'
-                                />
-                            </FormGroup>
-                        </Grid>
-                        <NavButton variant='back' link='/q5' clear={clear} />
-                        <Grid item xs={6}>
-                            <Button
-                                onClick={!errors && handleOnClick}
-                                type='submit'
-                                className={
-                                    !breakPoint
-                                        ? classes.navButton
-                                        : classes.navButtonLg
-                                }
-                                variant='contained'
-                            >
-                                Submit
-                            </Button>
-                        </Grid>
-                    </Grid>
-                </Box>
-            </Container>
-        </motion.div>
+                                <Grid item xs={6}>
+                                    <Button
+                                        type='submit'
+                                        className={
+                                            !breakPoint
+                                                ? classes.navButton
+                                                : classes.navButtonLg
+                                        }
+                                        variant='contained'
+                                    >
+                                        Submit
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </Container>
+                </motion.div>
+            ) : (
+                <motion.div
+                    variants={pageVariants}
+                    initial='hidden'
+                    animate='visible'
+                    exit='exit'
+                >
+                    <Container maxWidth='xs'>
+                        <Typography align='center' marginTop='20%' variant='h6'>
+                            Thank you for taking our questionnaire, we will be
+                            in touch soon.
+                        </Typography>
+                    </Container>
+                </motion.div>
+            )}
+        </div>
     );
 };
 
