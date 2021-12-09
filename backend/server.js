baefa86c620dde0131648b84e37799de6669b06a
aaronzor <mcmaaron@gmail.com>
2021-12-09 01:27:51 +0000

    Update access control headers

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', true);
     res.header(
         'Access-Control-Allow-Methods',
