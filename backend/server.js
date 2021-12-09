9abee9a1363032e54b28a90bda25c51f3988b584
aaron <mcmaaron@gmail.com>
2021-12-09 01:43:56 +0000

    Comment static route for local build

--- backend/server.js
+++ backend/server.js
@@ -87,17 +87,17 @@ app.use(errorHandler);
 
 const __dirname = path.resolve();
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/build')));
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-    app.get('*', (req, res) =>
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-    );
-} else {
-    app.get('/', (req, res) => {
-        res.send('API is running....');
-    });
-}
+//     app.get('*', (req, res) =>
+//         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+//     );
+// } else {
+//     app.get('/', (req, res) => {
+//         res.send('API is running....');
+//     });
+// }
 
 // Define port
 const PORT = process.env.PORT || 5000;
