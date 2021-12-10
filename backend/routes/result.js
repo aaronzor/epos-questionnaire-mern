0f58f1872133ecb8fec4fe46ff54128ae7ef512d
aaron <mcmaaron@gmail.com>
2021-12-10 23:09:05 +0000

    Remove redundant headers in route file

--- backend/routes/result.js
+++ backend/routes/result.js
@@ -19,31 +19,13 @@ import { protect, authorize } from '../middleware/auth.js';
 const router = express.Router();
 
 router
-    .route('/', (req, res) => {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Credentials', 'true');
-        res.setHeader('Access-Control-Max-Age', '1800');
-        res.setHeader('Access-Control-Allow-Headers', 'content-type');
-        res.setHeader(
-            'Access-Control-Allow-Methods',
-            'PUT, POST, GET, DELETE, PATCH, OPTIONS'
-        );
-    })
+    .route('/')
     .get(advancedResults(Result))
     .get(protect, authorize('admin'), getResults)
     .post(createResult);
 
 router
-    .route('/:id', (req, res) => {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Credentials', 'true');
-        res.setHeader('Access-Control-Max-Age', '1800');
-        res.setHeader('Access-Control-Allow-Headers', 'content-type');
-        res.setHeader(
-            'Access-Control-Allow-Methods',
-            'PUT, POST, GET, DELETE, PATCH, OPTIONS'
-        );
-    })
+    .route('/:id')
     .get(advancedResults(Result))
     .get(protect, authorize('admin'), getResult)
     .delete(deleteResult)
